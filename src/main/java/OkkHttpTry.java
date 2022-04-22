5ff0c2eb9354256e820736d10d28f5a854bc493a
morleybread <morleybread@gamil.com>
2022-04-22 21:54:50 +0800

    import

--- src/main/java/OkkHttpTry.java
+++ src/main/java/OkkHttpTry.java
@@ -1,16 +1,15 @@
 import okhttp3.*;
 import okhttp3.Headers.Builder;
-
 import okhttp3.internal.http2.Header;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class OkkHttpTry {
+    private final OkHttpClient client = new OkHttpClient();
 
 public String[] stringtoheader(String path) throws FileNotFoundException {
     File f=new File(path);
@@ -21,22 +20,53 @@ public String[] stringtoheader(String path) throws FileNotFoundException {
         stes.addAll(Arrays.asList(s));
     }
 //    String [] sts=stes.toArray(new String[0]);
-
   return stes.toArray(new String[0]);
 
 }
+    public void run() throws Exception {
+        Headers.Builder builder=new Headers.Builder();
+       Headers headers= builder.addAll(Headers.of(stringtoheader("src/main/resources/Header.txt"))).build();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/repos/square/okhttp/issues")
+                .headers(headers)
+                .build();
+//        System.out.println(request.headers().value(2));
+        System.out.println(request.headers());
+//        try (Response response = client.newCall(request).execute()) {
+//            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+////            System.out.println("Server: " + response.header("Server"));
+////            System.out.println("Date: " + response.header("Date"));
+////            System.out.println("Vary: " + response.headers("Vary"));
+//            System.out.println(response.body());
+//        }
+    }
 
+    public void parsebin() throws IOException {
+
+        FileInputStream fileInputStream=new FileInputStream(new File("src/main/resources/request_body.bin"));
+    int value;
+    while ((value=fileInputStream.read())!=-1){
+        System.out.println(value);
+
+    }
+
+
+    }
 
 
     public static void main(String[] args) throws Exception {
-     OkkHttpTry okkHttpTry=new OkkHttpTry();
-     String[] s= okkHttpTry.stringtoheader("src/main/resources/Header.txt");
-
-     for (String o:s){
-         System.out.println(o);
-     }
+           new OkkHttpTry().parsebin();
+//        System.out.println("++++++++++++++++++++=");
+//                String s="孔";
+//                byte[] bt=s.getBytes();
 //
-//        System.out.print(Headers.of("Content","text"));
+//        for (byte b : bt) {
+//            String temp = Integer.toHexString(b);
+//
+//            System.out.println(temp.substring(6).toUpperCase(Locale.ROOT));
+//
+//        }
+
 
 
     }
